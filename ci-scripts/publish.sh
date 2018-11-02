11f24f0a2d44918cd553669f07d2c8428c0d4dca
Daryl D'Souza <daryl-d@users.noreply.github.com>
2018-11-02 15:26:49 +1100

    ci-scripts fail fast

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 IMAGE_REPOSITORY='278521702583.dkr.ecr.us-west-2.amazonaws.com/iops/terraform-provider-cassandra'
 
 docker build -f Dockerfile -t ${IMAGE_REPOSITORY}:${IMAGE_TAG} .
