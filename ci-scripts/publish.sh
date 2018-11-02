a53b897910a13dbb1836299b95a60be5c0f49842
Daryl D'Souza <daryl-d@users.noreply.github.com>
2018-11-02 15:53:27 +1100

    Fixed up script, using var after it is set

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -4,11 +4,11 @@ set -ex
 
 IMAGE_REPOSITORY='278521702583.dkr.ecr.us-west-2.amazonaws.com/iops/terraform-provider-cassandra'
 
-docker build -f Dockerfile -t ${IMAGE_REPOSITORY}:${IMAGE_TAG} .
-
 if [ -n "$BUILDKITE" ]; then
   IMAGE_TAG="build-${BUILDKITE_BUILD_NUMBER}"
-  
+
+  docker build -f Dockerfile -t ${IMAGE_REPOSITORY}:${IMAGE_TAG} .
+
   docker push "${IMAGE_REPOSITORY}:${IMAGE_TAG}"
 
   if [ -n "$BUILDKITE_TAG" ]; then
