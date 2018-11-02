4588210c6f2f8761ef005d8e9db168253a7b675c
Daryl D'Souza <daryl-d@users.noreply.github.com>
2018-11-02 15:20:15 +1100

    Added in ci-scripts / setup for buildkite

---
+++ ci-scripts/publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+IMAGE_REPOSITORY='278521702583.dkr.ecr.us-west-2.amazonaws.com/iops/terraform-provider-cassandra'
+
+docker build -f Dockerfile -t ${IMAGE_REPOSITORY}:${IMAGE_TAG} .
+
+if [ -n "$BUILDKITE" ]; then
+  docker push "${IMAGE_REPOSITORY}:${IMAGE_TAG}"
+
+  if [ -n "$BUILDKITE_TAG" ]; then
+    docker tag "${IMAGE_REPOSITORY}:${IMAGE_TAG}" "${IMAGE_REPOSITORY}:${BUILDKITE_TAG}"
+    docker push "${IMAGE_REPOSITORY}:${BUILDKITE_TAG}"
+  fi
+fi
